2f1b4926310b0099ecdeb4e5aca25ee52c804650
Grzegorz Krawczyk <grzegorz.krawczyk.programista@gmail.com>
2026-01-08 21:04:32 +0100

    added comments to MainWindowController

--- src/main/java/controller/MainWindowController.java
+++ src/main/java/controller/MainWindowController.java
@@ -3,15 +3,26 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+/**
+ * Controller of the main window of the application.
+ */
 public class MainWindowController {
     @FXML
     private TextField currentLocalizationField;
 
+    /**
+     * Event listener triggered by clicking on check current localization button.
+     * It is used to get current localization of a device and to add this localization
+     * to input window.
+     */
     @FXML
     void checkCurrentLocalizationButtonAction() {
         System.out.println("Clicked check current localization");
     }
 
+    /**
+     * Event listener triggered by clicking on check weather for current localization button.
+     */
     @FXML
     void checkCurrentWeatherButtonAction() {
         System.out.println("Clicked check weather for current localization");
